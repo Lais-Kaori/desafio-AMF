b1d3ba6acf41ad92a8abf132926b25ad900e1ac3
Lais-Kaori <lais.kaorilk@gmail.com>
2022-10-12 23:54:49 -0300

    render and order functions

--- main.js
+++ main.js
@@ -1,38 +1,146 @@
-const url= "https://api.estagio.amfernandes.com.br/imoveis"
+const url = "https://api.estagio.amfernandes.com.br/imoveis";
 
-function getApi(){
-    axios.get(url)
-    .then(response => {
-        apiResult.textContent = JSON.stringify(response.data)
-        
-        console.log(response.data[0].planta.preco);
+function getApi() {
+  axios
+    .get(url)
+    .then((response) => {
+      apiResult.textContent = JSON.stringify(response.data);
+
+      console.log(response.data);
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
 }
 
+var mainList = [];
 
-function getBuildings(){
-    axios.get(url)
-    .then(response => {
-        neighborhood.textContent = response.data[0].bairro
-        city.textContent = response.data[0].cidade
-        price.textContent = response.data[0].planta.preco
+function renderBuildings() {
+  axios
+    .get(url)
+    .then((response) => {
+      var divBuildings = document.querySelector("#allBuildings");
+      divBuildings.innerHTML = "";
+      var buildingInfo = document.createElement("p");
+      for (var i = 0; i < response.data.length; i++) {
+        var currentItem = response.data[i];
+        var paragraph = document.createElement("li");
+
+        if (currentItem.planta != null) {
+          var obj = {
+            Nome: currentItem.nome,
+            Cidade: currentItem.cidade,
+            Bairro: currentItem.bairro,
+            Metragem: currentItem.planta.metragem,
+            Preço: currentItem.planta.preco,
+            Dormitórios: currentItem.planta.dorms,
+            Vagas: currentItem.planta.vagas,
+          };
+        } else {
+          var obj = {
+            Nome: currentItem.nome,
+            Cidade: currentItem.cidade,
+            Bairro: currentItem.bairro,
+            Metragem: null,
+            Preço: null,
+            Dormitórios: null,
+            Vagas: null,
+          };
+        }
+        mainList.push(obj);
+        paragraph.innerHTML = `
+                Nome: ${obj.Nome}<br>
+                Cidade: ${obj.Cidade}<br>
+                Bairro: ${obj.Bairro}<br>
+                Metragem: ${obj.Metragem}<br>
+                Preço: ${obj.Preço}<br>
+                Dormitórios: ${obj.Dormitórios}<br>
+                Vagas: ${obj.Vagas}<br>
+                <br>`;
+        divBuildings.appendChild(buildingInfo);
+        buildingInfo.appendChild(paragraph);
+      }
+      console.log(mainList);
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
 }
 
-function sortBairros(){
-    axios.get(url)
-    .then(response => {
-        let bairros = []
-        response.data.forEach(element => {
-            bairros.push(element.bairro);
-        });
-        console.log(bairros)
+function sortDistrict() {
+  axios
+    .get(url)
+    .then((response) => {
+      let district = [];
+      response.data.forEach((element) => {
+        district.push(element.bairro);
+      });
+      console.log(district);
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
 }
 
-//getApi()
-getBuildings()
-sortBairros()
+var orderedList = [];
+console.log(orderedList);
+
+function sortPriceDesc() {
+  axios
+    .get(url)
+    .then((response) => {
+      for (var i = 0; i < response.data.length; i++) {
+        var currentItem = response.data[i];
+        if (currentItem.planta != null) {
+          var objList = {
+            Nome: currentItem.nome,
+            Cidade: currentItem.cidade,
+            Bairro: currentItem.bairro,
+            Metragem: currentItem.planta.metragem,
+            Preço: currentItem.planta.preco,
+            Dormitórios: currentItem.planta.dorms,
+            Vagas: currentItem.planta.vagas,
+          };
+        } else {
+          var objList = {
+            Nome: currentItem.nome,
+            Cidade: currentItem.cidade,
+            Bairro: currentItem.bairro,
+            Metragem: null,
+            Preço: null,
+            Dormitórios: null,
+            Vagas: null,
+          };
+        }
+        orderedList.push(objList);
+      }
+      console.log(orderedList);
+      orderedList.sort((a, b) => b.Preço - a.Preço);
+      return orderedList;
+    })
+    .catch((error) => console.error(error));
+}
+
+function showOnScreen(element) {
+  axios
+    .get(url)
+    .then((response) => {
+      var divScreen = document.querySelector("#show");
+      var showInfo = document.createElement("p");
+      for (var i = 0; i < response.data.length; i++) {
+        var showParagraph = document.createElement("li");
+        showParagraph.innerHTML = `
+                Nome: ${element[i].Nome}<br>
+                Cidade: ${element[i].Cidade}<br>
+                Bairro: ${element[i].Bairro}<br>
+                Metragem: ${element[i].Metragem}<br>
+                Preço: ${element[i].Preço}<br>
+                Dormitórios: ${element[i].Dormitórios}<br>
+                Vagas: ${element[i].Vagas}<br>
+                <br>`;
+        divScreen.appendChild(showInfo);
+        showInfo.appendChild(showParagraph);
+      }
+    })
+    .catch((error) => console.error(error));
+}
+
+// getApi();
+// renderBuildings();
+// sortDistrict();
+sortPriceDesc();
+showOnScreen(orderedList);
