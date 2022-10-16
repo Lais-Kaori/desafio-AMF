02942df290974a004a57f1def572dbb8c4ac942b
Lais-Kaori <lais.kaorilk@gmail.com>
2022-10-16 02:37:39 -0300

    final project

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
 const url = "https://api.estagio.amfernandes.com.br/imoveis";
 
-function start(){
+function start() {
   renderButtons();
   listBuildings();
   selectSABuildings();
   selectSBBuildings();
   selectSCBuildings();
-  // orderNameAtoZ(mainList);
-  // orderNameZToA(mainList);
-  // orderPriceMaxToMin(mainList);
-  // orderDistrictAToZ(mainList);
-  showOnScreen(mainList);
-  // dropdownClick();
 }
 
 var mainList = [];
 var selectedSAList = [];
 var selectedSBList = [];
 var selectedSCList = [];
-var list = [];
+var currentList = mainList;
 
-function listBuildings(){
+function listBuildings() {
   axios
     .get(url)
     .then((response) => {
       for (var i = 0; i < response.data.length; i++) {
-        var currentItem = response.data[i];
+        let currentItem = response.data[i];
         if (currentItem.planta != null) {
           var obj = {
             Nome: currentItem.nome,
@@ -35,6 +29,7 @@ function listBuildings(){
             Preço: currentItem.planta.preco,
             Dormitórios: currentItem.planta.dorms,
             Vagas: currentItem.planta.vagas,
+            Imagem: currentItem.fachada,
           };
         } else {
           var obj = {
@@ -45,21 +40,23 @@ function listBuildings(){
             Preço: null,
             Dormitórios: null,
             Vagas: null,
+            Imagem: currentItem.fachada,
           };
         }
         mainList.push(obj);
       }
+      showOnScreen(mainList);
       return mainList;
     })
     .catch((error) => console.error(error));
 }
 
-function selectSABuildings(){
+function selectSABuildings() {
   axios
     .get(url)
     .then((response) => {
       for (var i = 0; i < response.data.length; i++) {
-        var currentItem = response.data[i];
+        let currentItem = response.data[i];
         if (currentItem.cidade === "Santo André") {
           if (currentItem.planta != null) {
             var obj = {
@@ -70,6 +67,7 @@ function selectSABuildings(){
               Preço: currentItem.planta.preco,
               Dormitórios: currentItem.planta.dorms,
               Vagas: currentItem.planta.vagas,
+              Imagem: currentItem.fachada,
             };
           } else {
             var obj = {
@@ -80,23 +78,23 @@ function selectSABuildings(){
               Preço: null,
               Dormitórios: null,
               Vagas: null,
+              Imagem: currentItem.fachada,
             };
           }
           selectedSAList.push(obj);
         }
       }
-      // console.log(selectedSAList);
       return selectedSAList;
     })
     .catch((error) => console.error(error));
 }
 
-function selectSBBuildings(){
+function selectSBBuildings() {
   axios
     .get(url)
     .then((response) => {
       for (var i = 0; i < response.data.length; i++) {
-        var currentItem = response.data[i];
+        let currentItem = response.data[i];
         if (currentItem.cidade === "São Bernardo do Campo") {
           if (currentItem.planta != null) {
             var obj = {
@@ -107,6 +105,7 @@ function selectSBBuildings(){
               Preço: currentItem.planta.preco,
               Dormitórios: currentItem.planta.dorms,
               Vagas: currentItem.planta.vagas,
+              Imagem: currentItem.fachada,
             };
           } else {
             var obj = {
@@ -117,23 +116,23 @@ function selectSBBuildings(){
               Preço: null,
               Dormitórios: null,
               Vagas: null,
+              Imagem: currentItem.fachada,
             };
           }
           selectedSBList.push(obj);
         }
       }
-      // console.log(selectedSBList);
       return selectedSBList;
     })
     .catch((error) => console.error(error));
 }
 
-function selectSCBuildings(){
+function selectSCBuildings() {
   axios
     .get(url)
     .then((response) => {
       for (var i = 0; i < response.data.length; i++) {
-        var currentItem = response.data[i];
+        let currentItem = response.data[i];
         if (currentItem.cidade === "São Caetano do Sul") {
           if (currentItem.planta != null) {
             var obj = {
@@ -144,6 +143,7 @@ function selectSCBuildings(){
               Preço: currentItem.planta.preco,
               Dormitórios: currentItem.planta.dorms,
               Vagas: currentItem.planta.vagas,
+              Imagem: currentItem.fachada,
             };
           } else {
             var obj = {
@@ -154,200 +154,187 @@ function selectSCBuildings(){
               Preço: null,
               Dormitórios: null,
               Vagas: null,
+              Imagem: currentItem.fachada,
             };
           }
           selectedSCList.push(obj);
         }
       }
-      // console.log(selectedSCList);
       return selectedSCList;
     })
     .catch((error) => console.error(error));
 }
 
-function orderPriceMaxToMin(){
-  axios
-    .get(url)
-    .then(
-      (response) => {
-        list.sort((a, b) => b.Preço - a.Preço);
-        showOnScreen(list);
-      })
-    .catch((error) => console.error(error));
-  }
-
-function orderPriceMinToMax(){
-  axios
-    .get(url)
-    .then(
-      (response) => {
-        list.sort((a, b) => a.Preço - b.Preço);
-        showOnScreen(list);
-      })
-    .catch((error) => console.error(error));
+function orderPriceMaxToMin() {
+  currentList.sort((a, b) => b.Preço - a.Preço);
+  showOnScreen(currentList);
 }
 
-function orderNameAToZ(){
-  axios
-    .get(url)
-    .then((response) => {
-      list.sort(function (x, y) {
-        let a = x.Nome.toUpperCase();
-        b = y.Nome.toUpperCase();
-        return a > b ? 1 : a == b ? 0 : -1;
-      });
-      showOnScreen(list);
-    })
-    .catch((error) => console.error(error));
+function orderPriceMinToMax() {
+  currentList.sort((a, b) => a.Preço - b.Preço);
+  showOnScreen(currentList);
 }
 
-function orderNameZToA(){
-  axios
-    .get(url)
-    .then((response) => {
-      list.sort(function (x, y) {
-        let a = x.Nome.toUpperCase();
-        b = y.Nome.toUpperCase();
-        return a < b ? 1 : a == b ? 0 : -1;
-      });
-      showOnScreen(list);
-    })
-    .catch((error) => console.error(error));
-  }
-
-function orderDistrictAToZ(){
-  axios
-    .get(url)
-    .then((response) => {
-      list.sort(function (x, y) {
-        let a = x.Bairro.toUpperCase();
-        b = y.Bairro.toUpperCase();
-        return a > b ? 1 : a == b ? 0 : -1;
-      });
-      showOnScreen(list);
-    })
-    .catch((error) => console.error(error));
+function orderNameAToZ() {
+  currentList.sort(function (x, y) {
+    let a = x.Nome.toUpperCase();
+    b = y.Nome.toUpperCase();
+    return a > b ? 1 : a == b ? 0 : -1;
+  });
+  showOnScreen(currentList);
 }
 
-function orderDistrictZToA(){
-  axios
-    .get(url)
-    .then((response) => {
-      list.sort(function (x, y) {
-        let a = x.Bairro.toUpperCase();
-        b = y.Bairro.toUpperCase();
-        return a < b ? 1 : a == b ? 0 : -1;
-      });
-      showOnScreen(list);
-    })
-    .catch((error) => console.error(error));
+function orderNameZToA() {
+  currentList.sort(function (x, y) {
+    let a = x.Nome.toUpperCase();
+    b = y.Nome.toUpperCase();
+    return a < b ? 1 : a == b ? 0 : -1;
+  });
+  showOnScreen(currentList);
 }
 
-function showOnScreen(element){
-  axios
-    .get(url)
-    .then((response) => {
-      var divScreen = document.querySelector("#show");
-      divScreen.innerHTML = "";
-      var showInfo = document.createElement("p");
-      showInfo.classList.add("info");
-      for (var i = 0; i < response.data.length; i++) {
-        var showParagraph = document.createElement("li");
-        showParagraph.innerHTML = `
-                Nome: ${element[i].Nome}<br>
-                Cidade: ${element[i].Cidade}<br>
-                Bairro: ${element[i].Bairro}<br>
-                Metragem: ${element[i].Metragem}<br>
-                Preço: ${element[i].Preço}<br>
-                Dormitórios: ${element[i].Dormitórios}<br>
-                Vagas: ${element[i].Vagas}<br>
-                <br>`;
-        divScreen.appendChild(showInfo);
-        showInfo.appendChild(showParagraph);
-      }
-    })
-    .catch((error) => console.error(error));
+function orderDistrictAToZ() {
+  currentList.sort(function (x, y) {
+    let a = x.Bairro.toUpperCase();
+    b = y.Bairro.toUpperCase();
+    return a > b ? 1 : a == b ? 0 : -1;
+  });
+  showOnScreen(currentList);
 }
 
-function renderButtons(){
-  var divButtons = document.querySelector(".buttons");
+function orderDistrictZToA() {
+  currentList.sort(function (x, y) {
+    let a = x.Bairro.toUpperCase();
+    b = y.Bairro.toUpperCase();
+    return a < b ? 1 : a == b ? 0 : -1;
+  });
+  showOnScreen(currentList);
+}
+
+const divScreen = document.querySelector("#show");
+
+const showOnScreen = (element) => {
+  const htmlString = element
+    .map((element) => {
+      return `
+    <li>
+    <p class="text"><b>Nome: </b> ${element.Nome}</br>
+    <b>Cidade: </b> ${element.Cidade}</br>
+    <b>Bairro: </b> ${element.Bairro}</br>
+    <b>Metragem: </b> ${element.Metragem}</br>
+    <b>Preço: </b> ${element.Preço}</br>
+    <b>Dormitórios: </b> ${element.Dormitórios}</br>
+    <b>Vagas: </b> ${element.Vagas}</br>
+    </p>
+    <img class="img" src="${element.Imagem}" alt="Imagem">
+    </li>
+    </br>`;
+    })
+    .join("");
+  divScreen.innerHTML = htmlString;
+};
+
+function renderButtons() {
+  let divButtons = document.querySelector(".buttons");
   divButtons.innerHTML = "";
 
-  var buttonTodos = createTodosButton();
-  var buttonSantoAndre = createSAButton();
-  var buttonSaoBernardo = createSBButton();
-  var buttonSaoCaetano = createSCButton();
+  let buttonTodos = createAllButton();
+  let buttonSantoAndre = createSAButton();
+  let buttonSaoBernardo = createSBButton();
+  let buttonSaoCaetano = createSCButton();
 
   divButtons.appendChild(buttonTodos);
   divButtons.appendChild(buttonSantoAndre);
   divButtons.appendChild(buttonSaoBernardo);
   divButtons.appendChild(buttonSaoCaetano);
-
 }
 
-function createTodosButton(){
-  var button = document.createElement("button");
+function createAllButton() {
+  let button = document.createElement("button");
   button.textContent = "Todos";
-  button.addEventListener("click", function(){showOnScreen(mainList)});
-  button.addEventListener("click", list = mainList);
-  console.log(list);
+  button.addEventListener("click", function () {
+    showOnScreen(mainList);
+  });
+  button.addEventListener("click", function () {
+    currentList = mainList;
+  });
   return button;
 }
 
-function createSAButton(){
-  var button = document.createElement("button");
+function createSAButton() {
+  let button = document.createElement("button");
   button.textContent = "Santo André";
-  button.addEventListener("click", function(){showOnScreen(selectedSAList)});
-  button.addEventListener("click", function(){list = selectedSAList});
-  console.log(list);
+  button.addEventListener("click", function () {
+    showOnScreen(selectedSAList);
+  });
+  button.addEventListener("click", function () {
+    currentList = selectedSAList;
+  });
   return button;
 }
 
-function createSBButton(){
-  var button = document.createElement("button");
+function createSBButton() {
+  let button = document.createElement("button");
   button.textContent = "São Bernardo";
-  button.addEventListener("click", function(){showOnScreen(selectedSBList)});
-  button.addEventListener("click", function(){list = selectedSBList});
-  console.log(list);
+  button.addEventListener("click", function () {
+    showOnScreen(selectedSBList);
+  });
+  button.addEventListener("click", function () {
+    currentList = selectedSBList;
+  });
   return button;
 }
 
-function createSCButton(){
-  var button = document.createElement("button");
+function createSCButton() {
+  let button = document.createElement("button");
   button.textContent = "São Caetano";
-  button.addEventListener("click", function(){showOnScreen(selectedSCList)});
-  button.addEventListener("click", function(){list = selectedSCList});
-  console.log(list);
+  button.addEventListener("click", function () {
+    showOnScreen(selectedSCList);
+  });
+  button.addEventListener("click", function () {
+    currentList = selectedSCList;
+  });
   return button;
 }
 
-// function define(element){
-//   list = element;
-// }
-
-// function sort(element) {
-//   orderPriceMaxToMin(element);
-//   showOnScreen(element);
-// }
-
-function dropdownClick (){
-  document.querySelector("#price-max").addEventListener("click", function(){console.log(list)});
+function dropdownClick() {
+  document.querySelector("#price-max").addEventListener("click", function () {
+  });
 }
-
 
 const selected = document.querySelector(".selected");
 const optionsContainer = document.querySelector(".options-container");
 const optionsList = document.querySelectorAll(".option");
 
+const downArrow = document.querySelector(".downArrow");
+const upArrow = document.querySelector(".upArrow");
+
 selected.addEventListener("click", () => {
   optionsContainer.classList.toggle("active");
-})
+  downArrow.classList.toggle("hideArrow");
+  upArrow.classList.toggle("showArrow");
+});
 
-optionsList.forEach( o =>{
+optionsList.forEach((o) => {
   o.addEventListener("click", () => {
     selected.innerHTML = o.querySelector("label").innerHTML;
     optionsContainer.classList.remove("active");
+    downArrow.classList.remove("hideArrow");
+    upArrow.classList.remove("showArrow");
   });
 });
 
-start()
+const searchBar = document.getElementById("searchBar");
+
+searchBar.addEventListener("keyup", (e) => {
+  const searchString = e.target.value;
+  const filteredList = currentList.filter((building) => {
+    return (
+      building.Nome.toLowerCase().includes(searchString) ||
+      building.Bairro.toLowerCase().includes(searchString)
+    );
+  });
+  showOnScreen(filteredList);
+});
+
+start();
